d814f7656a5e548aa467ddba2f06e231fa6ced8d
Kareem Saeed <52586356+kareem983@users.noreply.github.com>
2024-01-09 23:56:36 +0200

    - Create the database - Adding migration - Adding the User Controller - Adding the packages that the app needs it

---
+++ SportsBackend/Infrastructure/Data/SportsContext.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public  class SportsContext : IdentityDbContext<ApplicationUser>
+    {
+        public SportsContext(DbContextOptions<SportsContext> options): base(options)
+        {
+        } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        } 
+
+
+    }
+}
--- SportsBackend/SportsBackend/Program.cs
+++ SportsBackend/SportsBackend/Program.cs
@@ -1,3 +1,8 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +12,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<SportsContext>(options=> 
+                          options.UseSqlServer(builder.Configuration.GetConnectionString("SportsConnectionString")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<SportsContext>().AddDefaultTokenProviders();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(); 
     app.UseSwaggerUI();
 }
 
